c2a58a662d9aa63a20ec9cde36be19cc6aa26329
Lightening Gibson <lighteningibson@gmail.com>
2023-01-05 13:26:29 -0600

    commit tree 8a6dfc734fccd2c2ab6fdb3351d2797b6674dc5a

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,30 @@
-# your code goes here
+
+require 'pry'
+
+class Person 
+attr_reader :name, :bank_account, :happiness, :hygine
+
+
+def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygine = 8
+
+
+end
+
+def name
+
+    Person.name
+
+end
+
+
+
+end
+
+p = Person.new("Brooke")
+
+binding.pry
+
